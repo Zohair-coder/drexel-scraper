5ae1f49afe28cdf4f52fd9e56928435d031dd4ce
Zohair-coder <zohair.ul.hasan@gmail.com>
2023-09-20 00:13:09 -0400

    debugging

--- db_config.py
+++ db_config.py
@@ -4,5 +4,5 @@ DBNAME = os.environ["SCHEDULER_DB_DBNAME"]
 USER = os.environ["SCHEDULER_DB_USERNAME"]  # existing postgres user. change as needed
 PASSWORD = os.environ["SCHEDULER_DB_PG_PASSWORD"]  # password for postgres user. change as needed
 HOST = os.environ["SCHEDULER_DB_HOSTNAME"]  # default host for postgresql
-PORT = os.environ["SCHEUDLER_DB_PORT"]  # default port for postgresql
+PORT = os.environ["SCHEDULER_DB_PORT"]  # default port for postgresql
 DEFAULT_DB = os.environ["SCHEDULER_DB_DEFAULT_DB"]  # default database for postgresql used to create the new database if it doesn't exist
