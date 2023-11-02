d19044bbb1745dfba556f0a2791f451ca7f8b852
Zohair-coder <zohair.ul.hasan@gmail.com>
2023-11-02 16:44:51 -0400

    fixed bug

--- helpers.py
+++ helpers.py
@@ -6,26 +6,26 @@ def send_request(session: Session, url: str, method: str = "GET", json: dict = {
     try:
         resp = send_request_helper(session, url, method, json, headers)
         resp.raise_for_status()
-    except Exception as e:
+    except Exception as ex:
         retries = 10
         for _ in range(retries):
             try:
                 resp = send_request_helper(session, url, method, json, headers)
                 resp.raise_for_status()
-            except Exception as e:
-                if isinstance(e, HTTPError) and e.response.status_code == 429 and e.response.headers.get("Retry-After") is not None:
-                    if e.response.headers.get("Retry-After"):
-                        time.sleep(int(e.response.headers["Retry-After"]))
+            except Exception as inner_ex:
+                if isinstance(inner_ex, HTTPError) and inner_ex.response.status_code == 429 and inner_ex.response.headers.get("Retry-After") is not None:
+                    if inner_ex.response.headers.get("Retry-After"):
+                        time.sleep(int(inner_ex.response.headers["Retry-After"]))
                     else:
                         time.sleep(1)
                 continue
             return resp   
         
-        if isinstance(e, HTTPError):
-            resp_headers = e.response.headers
-            resp_text = e.response.text
-            raise Exception("Error sending HTTP request to {}.\nResponse headers: {}\nResponse text received: {}".format(url, resp_headers, resp_text)) from e
-        raise Exception("Error sending HTTP request to {}.".format(url)) from e
+        if isinstance(ex, HTTPError):
+            resp_headers = ex.response.headers
+            resp_text = ex.response.text
+            raise Exception("Error sending HTTP request to {}.\nResponse headers: {}\nResponse text received: {}".format(url, resp_headers, resp_text)) from ex
+        raise Exception("Error sending HTTP request to {}.".format(url)) from ex
     
     return resp 
 
