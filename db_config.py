555d91605ac3ccbdd923b491430d80a0bdaf8d19
Zohair-coder <zohair.ul.hasan@gmail.com>
2023-11-29 23:10:21 -0500

    removed 'change as needed' comments

--- db_config.py
+++ db_config.py
@@ -1,8 +1,8 @@
 import os
 
 DBNAME = os.environ.get("SCHEDULER_DB_DBNAME", "schedulerdb") # name of the database to connect to
-USER = os.environ.get("SCHEDULER_DB_USERNAME", "postgres")  # existing postgres user. change as needed
-PASSWORD = os.environ.get("SCHEDULER_DB_PG_PASSWORD", "")  # password for postgres user. change as needed
+USER = os.environ.get("SCHEDULER_DB_USERNAME", "postgres")  # existing postgres user
+PASSWORD = os.environ.get("SCHEDULER_DB_PG_PASSWORD", "")  # password for postgres user
 HOST = os.environ.get("SCHEDULER_DB_HOSTNAME", "localhost")  # default host for postgresql
 PORT = os.environ.get("SCHEDULER_DB_PORT", "5432")  # default port for postgresql
 DEFAULT_DB = os.environ.get("SCHEDULER_DB_DEFAULT_DB", "postgres")  # default database for postgresql used to create the new database if it doesn't exist
